81467f6f931a301e7b1dea18cdbf7cf62ef2eb92
Arthur Saito <arthur_saito1998@hotmail.com>
2022-12-08 01:32:13 -0300

    v1.2 - Added MovieList styles

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -35,12 +35,48 @@ return(
 }
 
 const MovieListContainer = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    h1{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        min-height: 110px;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 29px;
+        letter-spacing: 4%;
+        color: #293845;
+    }
 `
 
 const MovieListSection = styled.ul`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-around;
+
 `
 
 const MoviePost = styled.li`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 145px;
+    height: 209px;
+    box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 3px;
+    img{
+        width: 129px;
+        height: 193px;
+    }
+
 
 `
 export default Movies;
